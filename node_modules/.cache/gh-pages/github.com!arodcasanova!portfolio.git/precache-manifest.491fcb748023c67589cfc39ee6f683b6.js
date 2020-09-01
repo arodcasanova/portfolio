b6990a16606d34766f815bf6847c2264e66fcaf5
Adrian Rodriguez <arodriguez@cs.stanford.edu>
2020-09-01 14:03:50 -0700

    Add styling boilerplate

---
+++ node_modules/.cache/gh-pages/github.com!arodcasanova!portfolio.git/precache-manifest.491fcb748023c67589cfc39ee6f683b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5af17e3206ee9ad727ef6454d608bd6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "39fae55da29a9135531e",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bf79e0fc48d59fd198f7",
+    "url": "/portfolio/static/js/2.4c736102.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.4c736102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39fae55da29a9135531e",
+    "url": "/portfolio/static/js/main.9a43470d.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ src/components/Test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Type = styled.h1`
+	font-size: 3rem;
+`;
+
+const Test = () => (
+	<Type>hello, world!</Type>
+);
+
+export default Test;
