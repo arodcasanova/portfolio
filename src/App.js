c6692c2f2e7a008d740e5ded4ee87f3d45bef919
Adrian Rodriguez <arodriguez@cs.stanford.edu>
2020-09-01 11:21:10 -0700

    Add trivial change to homepage text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Adrian's Portfolio Preview
         </p>
         <a
           className="App-link"
